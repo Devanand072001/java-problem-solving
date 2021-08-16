be1475240174f15fc4be0b108d0cc1d51c727942
Devanand072001 <devanand.cs19@bitsathy.ac.in>
2021-08-16 16:37:16 +0530

    substring

--- src/Strings/GenerateSubString.java
+++ src/Strings/GenerateSubString.java
@@ -3,9 +3,9 @@ package Strings;
 public class GenerateSubString {
     public static void main(String[] args) {
         String string = "abcd";
-        for (int i = 0; i < string.length(); i ++){
-            for (int j = i+1; j <= string.length(); j++){
-                System.out.println(string.substring(i,j));
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                System.out.println(string.substring(i, j));
             }
         }
     }
